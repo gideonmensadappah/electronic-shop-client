58680c9d56db8b38a695ce777559bfc8d1289f19
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-24 22:59:18 +0300

    extracted side nav and futures component to deferent components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { NavBar } from "./components/navbar/navbar";
 import { DisplayProducts } from "./pages/displayHomeProducts/products";
+import { SideNav } from "./components/sideNav/sideNavlist";
+import { Sort } from "./components/sort/sort";
+
 function App() {
   return (
     <>
@@ -11,8 +14,12 @@ function App() {
           <NavBar />
         </div>
         <div className="App">
-          <div className="sideNav item">side Nav</div>
-          <div className="item-3 item">Futures...</div>
+          <div className="sideNav item">
+            <SideNav />
+          </div>
+          <div className="item-3 item ">
+            <Sort />
+          </div>
           <div className="products item">
             <DisplayProducts />
           </div>
