756affda248077961e87be1919fc291ab172b777
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-24 10:33:28 +0300

    implemented nav bar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+export const NavBar = () => {
+  const cartNumber = 2;
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          Electro <i className="fas fa-globe-africa"></i>
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/shop/cart">
+                <span span="cart-Number">{cartNumber}</span>
+                <i className="fas fa-shopping-cart cart-icon"> </i>
+              </Link>
+            </li>
+
+            <li className="nav-item active">
+              <Link className="nav-link" to="/user/login">
+                Login <span className="sr-only"></span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
