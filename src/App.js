f7731a57d605ba018a762e3608667931a7d44ce5
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-24 10:35:50 +0300

    created app design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { NavBar } from "./components/navbar/navbar";
+import { DisplayProducts } from "./pages/displayHomeProducts/products";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="header item ">
+          <NavBar />
+        </div>
+        <div className="App">
+          <div className="sideNav item">side Nav</div>
+          <div className="item-3 item">Futures...</div>
+          <div className="products item">
+            <DisplayProducts />
+          </div>
+        </div>
+      </Router>
+    </>
   );
 }
 
