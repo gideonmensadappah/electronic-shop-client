9c04f2357d877aca177cee4c0af70e8bdda66564
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-28 11:56:55 +0300

    Imported router dom. wrap the application with router.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -7,7 +8,9 @@ import "/Users/admin/Desktop/reactProjects/electronicShop/electronic-shop-client
 import "/Users/admin/Desktop/reactProjects/electronicShop/electronic-shop-client/node_modules/bootstrap/dist/css/bootstrap.css";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
