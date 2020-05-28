c3f7636d41b1c42b2deaa72ca3793c0f71b05fee
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-28 12:20:08 +0300

    implemented side bar menu

--- src/components/sideNav/sideNavMenu/sideNavMenu.js
+++ src/components/sideNav/sideNavMenu/sideNavMenu.js
@@ -1,21 +1,17 @@
 import React from "react";
 import "./sideBar.css";
+import { Link } from "react-router-dom";
+
 export const SideBarMenu = () => {
   return (
-    <div className="container">
+    <>
       <h4>Category</h4>
-      <div className="row">
-        <div className="col-4">
-          <nav className="navbar">
-            <ul>
-              <li>Bag</li>
-              <li>Men</li>
-              <li>Women</li>
-              <li>Kids</li>
-            </ul>
-          </nav>
-        </div>
+      <div className="sidenav">
+        <Link to="#about">About</Link>
+        <Link to="#services">Services</Link>
+        <Link to="#clients">Clients</Link>
+        <Link to="#contact">Contact</Link>
       </div>
-    </div>
+    </>
   );
 };
