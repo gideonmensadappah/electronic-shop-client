f6e23de3e0060c1d5ee14b92ed4f378d82c10af7
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-28 12:21:44 +0300

    implemented sort button(future)

--- src/components/sort/sort.js
+++ src/components/sort/sort.js
@@ -5,11 +5,11 @@ export const Sort = () => {
     <div className="container">
       <div className="row">
         <div className="col-2">
-          <select class="custom-select">
-            <option selected>Sort...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+          <select className="custom-select">
+            <option defaultValue>Sort...</option>
+            <option defaultValue="1">One</option>
+            <option defaultValue="2">Two</option>
+            <option defaultValue="3">Three</option>
           </select>
         </div>
       </div>
