5456df0dea7089b9e648c137017a95530d1fad1b
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-30 09:38:09 +0300

    changed route and component to desplay

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { LoginSystem } from "./components/loginSystem/login/login";
 import { shopCheckOut } from "./components/shop/checkout/checkOut";
 import { DisplayProducts } from "./components/shop/shopHome/displayHomeProducts/products";
 import { ProductModal } from "./components/products/ProductModal";
+import { ShopppingCart } from "./components/shop/cart/shoppingCart";
 
 function App() {
   return (
@@ -25,7 +26,7 @@ function App() {
           path="/shop/category/:category/:id/modal"
           component={ProductModal}
         />
-        <Route path="/shop/checkout" component={shopCheckOut} />
+        <Route path="/shop/cart" component={ShopppingCart} />
         <Route path="/user/login" component={LoginSystem} />
       </Switch>
     </>
