349ab6eb4581fbbf6f2b2e3e6a04fc53afe02eb1
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-30 09:39:52 +0300

    shop cart component - display current products in cart

--- src/components/shop/cart/shoppingCart.jsx
+++ src/components/shop/cart/shoppingCart.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
 export const ShopppingCart = () => {
-  return <div>shop cart...</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Image</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Image</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
