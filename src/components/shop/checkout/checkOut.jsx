a99209ec90c1cd6a577b2d1eb4f54b36d81747bd
Gideon Mensa <gideon123ewq@gmail.com>
2020-06-12 08:31:44 +0300

    added form to check out page

--- src/components/shop/checkout/checkOut.jsx
+++ src/components/shop/checkout/checkOut.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useCallback } from "react";
 
-export const shopCheckOut = () => {
-  return <div>Shop check out...</div>;
+export const ShopCheckOut = () => {
+  const userClickBuy = useCallback(() => {
+    alert("buy");
+  }, []);
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <div className="form-group">
+            <label>Name</label>
+            <input className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>Phone</label>
+            <input className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input className="form-control" />
+          </div>
+          <button className="btn btn-primary" onClick={userClickBuy}>
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
