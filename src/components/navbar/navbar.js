44fb846638098f07a5b3ee50e7c8bdf0bb0fff23
Gideon Mensa <gideon123ewq@gmail.com>
2020-06-12 08:26:09 +0300

    cart in nav updates when new item is added to cart

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-
-export const NavBar = () => {
-  const cartNumber = 2;
+import { connect } from "react-redux";
+const NavBar = ({ storeState }) => {
+  const { cartReducer } = storeState;
+  const cartNumber = cartReducer.length;
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -11,16 +12,16 @@ export const NavBar = () => {
           Electro <i className="fas fa-globe-africa"></i>
         </Link>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav ml-auto">
+          <ul className="navbar-nav nav-ul-right ml-auto">
             <li className="nav-item active">
-              <Link className="nav-link" to="/shop/cart">
-                <span span="cart-Number">{cartNumber}</span>
+              <Link className="nav-link shop-cart" to="/shop/cart">
+                <div span="cart-Number">{cartNumber}</div>
                 <i className="fas fa-shopping-cart cart-icon"> </i>
               </Link>
             </li>
 
             <li className="nav-item active">
-              <Link className="nav-link" to="/user/login">
+              <Link className="nav-link " to="/user/login">
                 Login <span className="sr-only"></span>
               </Link>
             </li>
@@ -30,3 +31,7 @@ export const NavBar = () => {
     </>
   );
 };
+const mapStateToProps = (storeState) => {
+  return { storeState };
+};
+export default connect(mapStateToProps)(NavBar);
