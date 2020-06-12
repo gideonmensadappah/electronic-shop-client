9a78f0d1576a6a3e334cc8ac382bffc3d0b787d9
Gideon Mensa <gideon123ewq@gmail.com>
2020-06-12 08:30:56 +0300

    added new action creators

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,6 +1,14 @@
 import { ACTION_TYPES } from "./actionTypes";
 
+export const addToCart = (productId) => ({
+  type: ACTION_TYPES.ADD_TO_CART,
+  productId,
+});
 export const paginate = (num) => ({
   type: ACTION_TYPES.PAGINATE,
   num,
+});
+export const getProducts = (products) => ({
+  type: ACTION_TYPES.GET_PRODUCTS,
+  products,
 });
